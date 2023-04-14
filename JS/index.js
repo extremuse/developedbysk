11ae32257422eea53e2cecb88067deb84548d082
extremuse <extremus.0@gmail.com>
2023-04-14 13:05:00 +0530

    second commit

--- JS/index.js
+++
@@ -1,8 +0,0 @@
-const hamburger=document.querySelectorAll(".hamburger");
-const bars=document.querySelectorAll(".bars");
-const menu=document.querySelectorAll(".menu");
-
-hamburger.addEventListener('click', () =>{
-    bars.classList.toggle('active');
-    
-})
